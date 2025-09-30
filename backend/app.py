3e015fa9f7a1eaf29338e69f82d463357fef18df
reetbajaj <167699423+reetbajaj@users.noreply.github.com>
2025-09-30 05:14:56 +0000

    canges in python

--- backend/app.py
+++ backend/app.py
@@ -34,5 +34,5 @@ def login():
         return jsonify({"message": "Invalid credentials"}), 401
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5050, debug=True)
+    app.run(host='0.0.0.0', port=5060, debug=True)
 
